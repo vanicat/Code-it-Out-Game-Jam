7bf9f7920ccf85d53ee2860b19fb1a0143286eea
Rémi Vanicat <vanicat@debian.org>
2022-04-17 07:47:09 +0200

    Add plateform "sprite"

--- level.py
+++ level.py
@@ -1,15 +1,37 @@
 import pyxel as px
 from lib import Pos
+from typing import Union
 
-class Plateforme:
-    def __init__(self, imgstart, pos) -> None:
-        self.imgstart = imgstart
+class Plateform:
+    def __init__(self, imgs: int, pos:Pos, width: int) -> None:
+        self.imgs = imgs
+        self.pos = px.TILE_SIZE * pos
+        self.width = width
+
+    def udpate(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        px.blt(self.pos.x, self.pos.y,
+            1, self.imgs.x, self.imgs.y, px.TILE_SIZE, px.TILE_SIZE)
+        for i in range(1, self.width - 1):
+            px.blt(self.pos.x + px.TILE_SIZE * i, self.pos.y,
+                1, self.imgs.x + px.TILE_SIZE, self.imgs.y, px.TILE_SIZE, px.TILE_SIZE)
+        px.blt(self.pos.x + (self.width - 1) * px.TILE_SIZE, self.pos.y,
+               1, self.imgs.x + px.TILE_SIZE * 2, self.imgs.y, px.TILE_SIZE, px.TILE_SIZE)
+        
+
+
 
 class Level:
-    def __init__(self, tilemap, goaly):
-        self.tilemap = tilemap
+    def __init__(self, tilemap: Union[px.Tilemap, int], goaly: float):
+        if isinstance(tilemap, int):
+            self.tilemap = px.tilemap(tilemap)
+        else:
+            self.tilemap = tilemap
         self.goaly = goaly
         self.pos = Pos(0, 0)
+        self.plateform = [Plateform(Pos(8, 0), self.pos, 3)]
         self.started = False
 
     def start(self):
@@ -17,6 +39,8 @@ class Level:
 
     def draw(self):
         px.bltm(0, 0, self.tilemap, self.pos.x, self.pos.y, px.width, px.height, 0)
+        for plt in self.plateform:
+            plt.draw()
 
     def udpate(self):
         if self.started:
