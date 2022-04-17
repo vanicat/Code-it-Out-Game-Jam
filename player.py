cb9e66e411b0ec242544c9585261d89c9262aada
Rémi Vanicat <vanicat@debian.org>
2022-04-17 09:15:17 +0200

    The player is falling.

--- player.py
+++ player.py
@@ -1,18 +1,29 @@
 import pyxel as px
 
+from lib import Pos
+
 class Player:
     IMGX = 0
     IMGY = 0
     SIZE = px.TILE_SIZE * 2
+    GRAVITY = Pos(0, 2)
 
     def __init__(self, pos):
         self.pos = pos
 
     def update(self):
-        pass
+        if self.falling():
+            self.pos += self.GRAVITY
+
+    def falling(self):
+        return True
 
     def draw(self):
         px.blt(self.pos.x, self.pos.y, 0, self.IMGX, self.IMGY, self.SIZE, self.SIZE, px.COLOR_BLACK)
 
 
-    
+
+if __name__ == "__main__":
+    import main
+
+    main.App()
