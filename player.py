fd00c512fa626fd8d31e54a00d22af32f8e181f8
Rémi Vanicat <vanicat@debian.org>
2022-04-17 11:12:02 +0200

    Add jump and double jump

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pyxel as px
+#from level import Level
 
 from lib import Pos
 
@@ -7,6 +8,9 @@ class Player:
     IMGY = 0
     SIZE = px.TILE_SIZE * 2
     GRAVITY = Pos(0, 2)
+    JUMP = Pos(0, -6)
+    NB_JUMP = 2
+    JUMP_GRAVITY = 0.5
 
     def __init__(self, pos, level: "Level"):
         self.pos = pos
@@ -14,19 +18,32 @@ class Player:
         self.rect = self
         self.speed = Pos(2, 0)
         self.jump = None
+        self.jump_left = self.NB_JUMP
 
     def update(self):
         self.falling = True
-        
+
         for plt in self.level.plateform:
             if plt.rect.under(self):
                 self.falling = False
+                self.jump = None
+                self.jump_left = self.NB_JUMP
                 self.bottom = plt.rect.top
-        
+
         if self.falling:
             self.pos += self.GRAVITY
 
         self.pos += self.speed
+
+        if px.btnp(px.KEY_SPACE) and (not self.falling or self.jump_left > 0):
+            self.jump = self.JUMP.copy()
+            self.jump_left -= 1
+
+        if self.jump is not None:
+            self.pos += self.jump
+            self.jump.y += self.JUMP_GRAVITY
+            if self.jump.y > 0:
+                self.jump = None
 
     @property
     def bottom(self):
@@ -50,7 +67,6 @@ class Player:
 
     def draw(self):
         px.blt(self.pos.x, self.pos.y, 0, self.IMGX, self.IMGY, self.SIZE, self.SIZE, px.COLOR_BLACK)
-
 
 
 if __name__ == "__main__":
