78285153904fd2876c31cfb2b0f525bfef178918
Rémi Vanicat <vanicat@debian.org>
2022-04-17 13:30:46 +0200

    Add killer plateform

--- level.py
+++ level.py
@@ -1,3 +1,4 @@
+from turtle import pos
 import pyxel as px
 from typing import Union
 
@@ -6,8 +7,8 @@ from player import Player
 
 class Plateform:
     def __init__(self, imgs: Pos, pos:Pos, width: int) -> None:
-        self.imgs = imgs
-        self.pos = pos
+        self.imgs = px.TILE_SIZE * imgs
+        self.pos = px.TILE_SIZE * pos
         self.width = width
         self.rect = Rectangle(self.pos, self.width * px.TILE_SIZE, px.TILE_SIZE)
 
@@ -24,7 +25,11 @@ class Plateform:
                1, self.imgs.x + px.TILE_SIZE * 2, self.imgs.y, px.TILE_SIZE, px.TILE_SIZE)
 
 
+class killer:
+    def __init__(self) -> None:
+        self.pos = pos
 class Level:
+    IGNORE_TILE = [(0, 0), (4, 0)]
     def __init__(self, tilemap: Union[px.Tilemap, int], goaly: float):
         if isinstance(tilemap, int):
             self.tilemap = px.tilemap(tilemap)
@@ -34,36 +39,48 @@ class Level:
         self.pos = Pos(0, 0)
 
         self.plateform = []
+        self.killer = []
 
         self.init_from_tile_map()
 
     def init_from_tile_map(self):
         plt_start = False
-        for u in range(px.TILEMAP_SIZE):
-            for v in range(px.TILEMAP_SIZE):
+        for v in range(px.TILEMAP_SIZE):
+            for u in range(px.TILEMAP_SIZE):
                 tile = self.tilemap.pget(u, v)
-                if tile == (0, 0):pass
+                if tile in self.IGNORE_TILE:pass
+
                 elif tile == (1, 0):
                     # start of plateform
-                    plt_start = (u, v)
+                    assert not plt_start, f"starting twice ({u}, {v})"
+                    plt_start = Pos(u, v)
                     plt_width = 1
                 elif tile == (2, 0):
                     # plateform continuing
-                    assert plt_start
+                    assert plt_start, f"continue without start ({u}, {v})"
                     plt_width += 1
                 elif tile == (3, 0):
-                    assert plt_start
+                    assert plt_start, f"end without start ({u}, {v})"
                     # stop of a plateform
-                    self.plateform.append(Plateform(Pos(8, 0), px.TILE_SIZE * posT(plt_start), plt_width))
+                    self.plateform.append(Plateform(Pos(1, 0), plt_start, plt_width))
                     plt_start = False
+
+                elif tile == (4, 1):
+                    self.killer.append(Plateform(Pos(4, 1), Pos(u, v), 255))
+
                 elif tile == (0, 1):
-                    self.player = Player(px.TILE_SIZE * Pos(u, v), self)
+                    self.init_player = px.TILE_SIZE * Pos(u, v)
+                    self.player = Player(self.init_player, self)
                 else:
                     print("unkwon tile", tile, "at", u, ",", v)
 
     def draw(self):
         for plt in self.plateform:
             plt.draw()
+
+        for kill in self.killer:
+            kill.draw()
+
         self.player.draw()
 
     def udpate(self):
